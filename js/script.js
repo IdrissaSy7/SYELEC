9a6ea05e8331679d3e210310349173893bc7c494
IdrissaSy7 <Kolo785@gmail.com>
2023-02-22 21:42:57 +0100

    Modif

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 const toggleBtn = document.getElementById("theme");
 
+// Vérifier si un thème a été enregistré dans le localStorage
+if (localStorage.getItem("theme")) {
+  let theme = localStorage.getItem("theme");
+  document.body.classList.add(theme + "-theme");
+} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  // Appliquer le thème sombre par défaut si le navigateur est en mode sombre
+  document.body.classList.add("dark-theme");
+} else {
+  // Appliquer le thème clair par défaut si le navigateur est en mode clair
+  document.body.classList.add("light-theme");
+}
+
 toggleBtn.addEventListener("click", function () {
   // Vérifier le thème actuel
   let currentTheme = document.body.classList.contains("dark-theme")
@@ -12,17 +24,6 @@ toggleBtn.addEventListener("click", function () {
   document.body.classList.replace(currentTheme + "-theme", newTheme + "-theme");
   localStorage.setItem("theme", newTheme);
 });
-// Vérifier si un thème a été enregistré dans le localStorage
-if (localStorage.getItem("theme")) {
-  let theme = localStorage.getItem("theme");
-  document.body.classList.add(theme + "-theme");
-} else if (prefersDarkScheme.matches) {
-  // Appliquer le thème sombre par défaut si le navigateur est en mode sombre
-  document.body.classList.add("dark-theme");
-} else {
-  // Appliquer le thème clair par défaut si le navigateur est en mode clair
-  document.body.classList.add("light-theme");
-}
 
 // Menu déroulant
 const menu = document.querySelector("#menu-icon");
